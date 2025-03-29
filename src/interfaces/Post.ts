5190b465d72c9d13e4a75d92034feb324108fdd1
Sirasit Tanrattanawong <might.la.fr@gmail.com>
2025-03-29 10:29:19 +0700

    add-interface

---
+++ src/interfaces/Post.ts
@@ -0,0 +1,12 @@
+import { Character } from "./Character";
+export type DayType = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+export interface Post {
+	id: string;
+	gameId: string;
+    day: DayType;
+    character: Character;
+    text: string;
+    numLikes: number;
+    image?: string;
+}

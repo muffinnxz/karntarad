317467415f4080d49f8fd17221448f4d829f07e6
muffinnxz <mr.muffinnxz@gmail.com>
2025-03-29 21:46:37 +0700

    feat: change post interface

--- src/interfaces/Post.ts
+++ src/interfaces/Post.ts
@@ -1,11 +1,14 @@
-import { Character } from "./Character";
-import { User } from "./User";
+export interface PostCreator {
+  name: string;
+  username: string;
+  image?: string;
+}
+
 export interface Post {
   id: string;
   gameId: string;
   day: number;
-  user?: User; // post from the real user
-  character?: Character; // post from AI character
+  creator: PostCreator;
   text: string;
   numLikes: number;
   image?: string;

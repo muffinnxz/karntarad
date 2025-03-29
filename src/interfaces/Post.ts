a9e1b501e274e9bdbb6c06d563eeda11608b47dc
Sirasit Tanrattanawong <might.la.fr@gmail.com>
2025-03-29 11:11:21 +0700

    update post interface

--- src/interfaces/Post.ts
+++ src/interfaces/Post.ts
@@ -1,11 +1,13 @@
 import { Character } from "./Character";
+import { User } from "firebase/auth";
 export type DayType = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
 export interface Post {
 	id: string;
 	gameId: string;
     day: DayType;
-    character: Character;
+    user?: User;        // post from the real user
+    character?: Character; // post from AI character
     text: string;
     numLikes: number;
     image?: string;
